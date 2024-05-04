50884d540a06d68871d79a93446b77640bc58301
H3xadecimal <h3xadecimal@afternode.cn>
2024-05-04 21:51:26 +0800

    perf: cleanup again

--- src/main/java/org/eu/pcraft/template/ConfigTemplate.java
+++ src/main/java/org/eu/pcraft/template/ConfigTemplate.java
@@ -3,12 +3,12 @@ import lombok.Getter;
 import org.bukkit.configuration.file.FileConfiguration;
 import org.eu.pcraft.pepperminecart.PepperMinecart;
 
+@Getter
 public class ConfigTemplate {
     public void loadConfig() {
         FileConfiguration conf = PepperMinecart.getInstance().getConfig();
         enableCustomInteract = conf.getBoolean("enableCustomInteract");
     }
 
-    @Getter
     private boolean enableCustomInteract;
 }
